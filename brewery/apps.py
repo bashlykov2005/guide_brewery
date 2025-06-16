c8f59844756bc7cfd9a5907cb9a80741261f0f58
Alexander Bashlykov <127608153+bashlykov2005@users.noreply.github.com>
2025-06-16 22:52:20 +0500

    Delete brewery directory

--- brewery/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BreweryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'brewery'
-    verbose_name = "breweries"
